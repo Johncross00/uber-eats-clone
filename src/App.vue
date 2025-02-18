ea1c17f76fb3f996654e5a862ebda1c871bb4e8f
johncross00 <johncross.ktm@gmail.com>
2025-02-18 11:04:31 +0000

    Ajout d'un fichier de réinitialisation CSS et importation de la police Roboto dans App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@ import TheWelcome from './components/TheWelcome.vue'
 </template>
 
 <style scoped>
+@import url('./assets/styles/reset.css');
+@import url('https: //fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
 header {
   line-height: 1.5;
 }
