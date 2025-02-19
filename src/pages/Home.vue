dcf36193a870d78505b2c7fe2af3f56c069db464
johncross00 <johncross.ktm@gmail.com>
2025-02-19 11:14:32 +0000

    Ajout d'une en-tête avec logo et champ de recherche dans Home.vue, et mise à jour du style CSS pour le composant Home.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="home">
-        <RestaurantRow v-for="data in data_restaurant" :key="data" :three_restaurent="data"/>
+        <div class="header">
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg"
+                alt="Logo de Uber Eats">
+            <input type="text" placeholder="De quoi avez vous envie?">
+        </div>
+        <div class="banner">
+
+        </div>
+        <RestaurantRow v-for="data in data_restaurant" :key="data" :three_restaurent="data" />
     </div>
 </template>
 
@@ -8,10 +16,12 @@
 import { ref, onMounted } from 'vue';
 import RestaurantRow from '../components/RestaurantRow.vue'
 import bd from '../bd.js'
-console.log(bd)
+console.log( bd )
 
-class Restaurant{
-    constructor(name, note, image, drive_time){
+class Restaurant
+{
+    constructor ( name, note, image, drive_time )
+    {
         this.name = name
         this.note = note
         this.image = image
@@ -19,28 +29,67 @@ class Restaurant{
     }
 }
 
-let data_restaurant = ref([])
-const makeDataRestaurant = () => {
-    let three_restaurant =[]
+let data_restaurant = ref( [] )
+const makeDataRestaurant = () =>
+{
+    let three_restaurant = []
 
-    for(const restaurant of bd){
-        const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time)
-        
-        if(three_restaurant.length == 2){
+    for ( const restaurant of bd )
+    {
+        const new_restaurant = new Restaurant( restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time )
+
+        if ( three_restaurant.length == 2 )
+        {
             three_restaurant.push( new_restaurant )
-            data_restaurant.value.push( three_restaurant)
+            data_restaurant.value.push( three_restaurant )
             three_restaurant = []
         }
-        else{
+        else
+        {
             three_restaurant.push( new_restaurant )
         }
     }
-    console.log(data_restaurant)
+    console.log( data_restaurant )
 }
-onMounted(makeDataRestaurant)
+onMounted( makeDataRestaurant )
 
 </script>
 
-<style scoped>
-  
+<style scoped lang="scss">
+.home {
+    .header {
+        height: 120px;
+        width: 100%;
+        display: flex;
+        // flex-direction: column-reverse;
+        justify-content: space-between;
+        align-items: center;
+
+        // background-color: #eeeeee;
+        // padding: 50px;
+        img {
+            width: 200px;
+        }
+
+        input {
+            background-color: #f6f6f6;
+            border-radius: 15px;
+            border: none;
+            height: 60px;
+            width: 400px;
+            padding-left: 20px;
+            box-shadow: 0px 5px 5px #eeeeee;
+            font-size: 1.2rem;
+            outline: none;
+        }
+    }
+
+    .banner {
+        height: 245px;
+        width: 100%;
+        background-image: url("https://th.bing.com/th/id/R.9c14304ad1375a3c79d400e0c7743646?rik=Z0Q98S4KyhOwLA&pid=ImgRaw&r=0");
+        background-size: full;
+        background-position: center;
+    }
+}
 </style>
